397e4f47dd49182b1a4b82f046b9874021ff5cde
Tomasz Bielecki <szkoleniegoit@gmail.com>
2023-01-03 10:43:23 +0100

    completed phonebook task

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,6 @@ class App extends Component {
   };
 
   resetForm = event => {
-    console.log('resetForm');
     event.target.name.value = '';
     event.target.number.value = '';
     this.setState({
@@ -39,7 +38,6 @@ class App extends Component {
   };
 
   setContactsToLocaleStorage = () => {
-    console.log('setContactsToLocaleStorage');
     try {
       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
     } catch (error) {
@@ -48,7 +46,6 @@ class App extends Component {
   };
 
   addContact = evt => {
-    console.log('addContact');
     evt.preventDefault();
     const valueName = evt.currentTarget.elements.name.value;
     const valueNumber = evt.currentTarget.elements.number.value;
@@ -67,9 +64,6 @@ class App extends Component {
               },
             ],
           };
-        },
-        () => {
-          this.setContactsToLocaleStorage();
         }
       );
     }
@@ -77,7 +71,6 @@ class App extends Component {
   };
 
   addCurrentValue = event => {
-    console.log('addCurrentValue');
     const name = event.target.name;
     const value = event.target.value;
     this.setState({
@@ -86,7 +79,6 @@ class App extends Component {
   };
 
   filterContacts = () => {
-    console.log('filterContacts');
     const filteredArray = [];
     this.state.contacts.filter(element => {
       if (element.name.includes(this.state.filter)) {
@@ -98,7 +90,6 @@ class App extends Component {
   };
 
   deleteContact = event => {
-    console.log('deleteContact');
     let contactIndex;
     this.state.contacts.map((element, index) => {
       if (event.target.parentNode.textContent.includes(element.name)) {
@@ -134,18 +125,13 @@ class App extends Component {
     }
   };
 
-  componentDidUpdate(prevProps, prevState, snapshot) {
+  componentDidUpdate(prevProps, prevState) {
     if(prevState.contacts.length!==this.state.contacts.length){
-      console.log("change contacts");
-      
-    }
-    if(prevState.filter!==this.state.filter){
-      console.log("change filter");
+      this.setContactsToLocaleStorage();
     }
   }
 
   render() {
-    console.log("Rendering!!!");
     return (
       <>
         <h1>Phonebook</h1>
